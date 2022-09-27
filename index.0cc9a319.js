e5270fd7f4b3c32c9d1477d1c0131196b2cd62ea
AlexMUa7 <111360769+AlexMUa7@users.noreply.github.com>
2022-09-27 08:02:32 +0000

    Deploying to gh-pages from @ AlexMUa7/brain-power@d436304892b9c317af7b7ccd30ffe54d3edcc35b 🚀

---
+++ index.0cc9a319.js
@@ -0,0 +1,2 @@
+!function(){var e={openvideomodalBtn:document.querySelector("[data-modal-open1]"),closevideomodalBtn:document.querySelector("[data-modal-1close]"),videomodal:document.querySelector("[data-modal1]")};function o(){e.videomodal.classList.toggle("is-hidden")}e.openvideomodalBtn.addEventListener("click",o),e.closevideomodalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.0cc9a319.js.map
